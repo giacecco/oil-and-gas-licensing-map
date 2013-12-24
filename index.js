6f62c46f089d82aeeb604de4e15eda6ba83b54a9
Gianfranco Cecconi <gianfranco.cecconi@digitalcontraptionsimaginarium.co.uk>
2013-12-24 10:48:24 +0000

    Put the layers configuration in an external json file

--- index.js
+++ index.js
@@ -49,6 +49,7 @@ var makeGeoJSON = function (dataFile, callback) {
 	});
 };
 
+/*
 var getLicenceColour = function (value) {
 	// Same colours as the original map
 	var colour = "#EEFCED";
@@ -73,46 +74,36 @@ var style = function (feature) {
         fillOpacity: 0.7
     };
 }
-
-var style2 = function (feature) {
-    return {
-        fillColor: getLicenceColour(feature.properties.license),
-        weight: 2,
-        opacity: 1,
-        color: 'red',
-        dashArray: '3',
-        fillOpacity: 0.7
-    };
-}
+*/
 
 // Derived from example at http://switch2osm.org/using-tiles/getting-started-with-leaflet/
 var initMap = function () {
-	var existingLicences,
-		dec2013Offering,
-		areasUnderConsideration;
-	async.parallel([
-		function (callback) {
-			d3.json("existingLicences.json", function(data) { existingLicences = data; callback(null); });
-		},
-		function (callback) {
-			d3.json("dec2013Offering.json", function(data) { dec2013Offering = data; callback(null); });
-		},
-		function (callback) {
-			makeGeoJSON("areasUnderConsideration.csv", function (err, a) { areasUnderConsideration = a; callback(err); });
-		},		
-	], function (err) {
-		// set up the map
-		map = new L.Map('map');
-		// create the tile layer with correct attribution
-		var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-		var osmAttrib='Map data © OpenStreetMap contributors';
-		var osm = new L.TileLayer(osmUrl, { minZoom: 1, maxZoom: 12, attribution: osmAttrib });		
-		// start the map in South-East England
-		map.setView(new L.LatLng(55.6, -3.0), 7);
-		map.addLayer(osm);
-		L.geoJson(areasUnderConsideration, {style: style}).addTo(map);
-		L.geoJson(existingLicences, {style: style}).addTo(map);
-		L.geoJson(dec2013Offering, {style: style2}).addTo(map);
+	var configuration; 
+	d3.json("configuration.json", function (c) { 
+		configuration = c;
+		async.each(_.keys(configuration.layers), function (layerName, callback) {
+			switch (configuration.layers[layerName].dataType) {
+				case "geojson":
+					d3.json(configuration.layers[layerName].dataFile, function(data) { configuration.layers[layerName].geoJSON = data; callback(null); });
+					break;
+				case "csv":
+					makeGeoJSON(configuration.layers[layerName].dataFile, function (err, data) { configuration.layers[layerName].geoJSON = data; callback(err); });
+					break;
+			}
+		}, function (err) {
+			// set up the map
+			map = new L.Map('map');
+			// create the tile layer with correct attribution
+			var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+			var osmAttrib='Map data © OpenStreetMap contributors';
+			var osm = new L.TileLayer(osmUrl, { minZoom: 1, maxZoom: 12, attribution: osmAttrib });		
+			// start the map in South-East England
+			map.setView(new L.LatLng(55.6, -3.0), 7);
+			map.addLayer(osm);
+			_.each(_.values(configuration.layers), function (l) {
+				L.geoJson(l.geoJSON, {style: function (feature) { return l.style; } }).addTo(map);
+			});
+		});
 	});
 }
 
